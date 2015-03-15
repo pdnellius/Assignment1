901c097ac789581eb04c97fcb083c9373dc1e8d7
Pete Nellius <peternellius@gmail.com>
2015-03-15 08:43:47 -0400

    Adding initial version of storyboard project

---
+++ src/Lesson01/ModalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModalViewController.swift
+//  Lesson01
+//
+//  Created by Pete Nellius on 3/14/15.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
